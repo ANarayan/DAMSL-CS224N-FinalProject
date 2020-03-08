d0871ed9ab529028eacc6f3d2894a2a50b6bb6a4
Joshua Hedtke <hedtke@gridspace.com>
2020-03-08 12:14:33 -0700

    minor maml changes

--- maml/maml.py
+++ maml/maml.py
@@ -22,14 +22,16 @@ import torch.nn.functional as F
 import torch.nn as nn
 from torch.utils.tensorboard import SummaryWriter
 
+BASE_DIR = Path.cwd().parents[1]
 
-def train_and_eval_maml(model, trainandtest_data_files, eval_data, optimizer, meta_optimizer, model_dir, data_dir,
+
+def train_and_eval_maml(model, trainandval_data_files, eval_data, optimizer, meta_optimizer, model_dir, data_dir,
         training_params, dataset_params, meta_training_params, model_params, device):
     """
     Train on n - 1 domains, eval and test on nth
 
     Args:
-        trainandtest_data_files: dict {domain_name: {train_file_path: fp, test_file_path: fp}
+        trainandval_data_files: dict {domain_name: {train_file_path: fp, val_file_path: fp}
     """
     #TODO: add num grad steps for inner loop hyperparam
 
@@ -37,15 +39,15 @@ def train_and_eval_maml(model, trainandtest_data_files, eval_data, optimizer, me
 
     # Load training and test sets for n - 1 domains
 
-    domain_names = trainandtest_data_files.keys()
-    tasks_train = [DialoguesDataset(Path(data_dir) / trainandtest_data_files[domain]['train_file_path']) for domain in
+    domain_names = trainandval_data_files.keys()
+    tasks_train = [DialoguesDataset(Path(data_dir) / trainandval_data_files[domain]['train_file_path']) for domain in
             domain_names]
-    tasks_test = [DialoguesDataset(Path(data_dir) / trainandtest_data_files[domain]['test_file_path']) for domain in
+    tasks_val = [DialoguesDataset(Path(data_dir) / trainandval_data_files[domain]['val_file_path']) for domain in
             domain_names]
     train_generators = [train_dataset.data_iterator(meta_training_params['meta_inner_batch_size'], shuffle=True) for train_dataset in tasks_train]
-    test_generators = [test_dataset.data_iterator(meta_training_params['meta_inner_batch_size'], shuffle=True) for
-            test_dataset in tasks_test]
-    generators = list(zip(train_generators, test_generators))
+    val_generators = [val_dataset.data_iterator(meta_training_params['meta_inner_batch_size'], shuffle=True) for
+            val_dataset in tasks_val]
+    generators = list(zip(train_generators, val_generators))
 
     pos_weights = torch.tensor([training_params['pos_weighting']] * model_params['num_slots'])
     loss_func = nn.BCEWithLogitsLoss(pos_weight=pos_weights, reduction='none')
@@ -54,6 +56,11 @@ def train_and_eval_maml(model, trainandtest_data_files, eval_data, optimizer, me
 
     total_meta_loss = 0
 
+    best_val_acc = 0.0
+    prev_val_acc = 0.0
+    best_loss = 1e1000
+    prev_val_loss = 1e1000
+    early_stopping_count = 0
     #Train
     for meta_epoch in trange(meta_training_params['meta_epochs']):
 
@@ -63,16 +70,16 @@ def train_and_eval_maml(model, trainandtest_data_files, eval_data, optimizer, me
 
         meta_batch_loss = 0
 
-        task_test_losses_before_update = []
-        task_test_losses_after_update = []
-        task_test_outputs = []
+        meta_test_losses_before_update = []
+        meta_test_losses_after_update = []
+        meta_test_outputs = []
 
         #Sample batch
         batch_of_tasks = random.sample(generators, meta_training_params['meta_outer_batch_size'])
         for t in batch_of_tasks:
             try:
                 turn_and_cand_train, cand_label_train = next(t[0])
-                turn_and_cand_test, cand_label_test = next(t[1])
+                turn_and_cand_test, cand_label_test = next(t[0])
             except StopIteration:
                 generators.pop(generators.index(t))
                 if not generators:
@@ -80,8 +87,8 @@ def train_and_eval_maml(model, trainandtest_data_files, eval_data, optimizer, me
                 else:
                     continue
             output_test = model(turn_and_cand_test)
-            task_test_loss_before_update = loss_func(output_test, cand_label_test).sum()
-            task_test_losses_before_update.append(task_test_loss_before_update.item())
+            meta_test_loss_before_update = loss_func(output_test, cand_label_test).sum()
+            meta_test_losses_before_update.append(meta_test_loss_before_update.item())
 
             output_train = model(turn_and_cand_train)
             task_train_loss = loss_func(output_train, cand_label_train).sum()
@@ -91,11 +98,11 @@ def train_and_eval_maml(model, trainandtest_data_files, eval_data, optimizer, me
             optimizer.step()
 
             output_test = model(turn_and_cand_test)
-            task_test_outputs.append(output_test)
-            task_test_loss_after_update = loss_func(output_test, cand_label_test).sum()
-            task_test_losses_after_update.append(task_test_loss_after_update.item())
+            meta_test_outputs.append(output_test)
+            meta_test_loss_after_update = loss_func(output_test, cand_label_test).sum()
+            meta_test_losses_after_update.append(meta_test_loss_after_update.item())
 
-            meta_batch_loss += task_test_loss_after_update
+            meta_batch_loss += meta_test_loss_after_update
 
             #Restore weights to before any task loop updates
 
@@ -112,16 +119,44 @@ def train_and_eval_maml(model, trainandtest_data_files, eval_data, optimizer, me
         logging.info('Meta batch loss: {}'.format(meta_batch_loss))
 
 
-        logging.info('Average inner loss before inner updates : {}'.format(np.mean(task_test_losses_before_update)))
-        logging.info('Average inner loss after inner updates : {}'.format(np.mean(task_test_losses_after_update)))
+        logging.info('Average inner loss before inner updates : {}'.format(np.mean(meta_test_losses_before_update)))
+        logging.info('Average inner loss after inner updates : {}'.format(np.mean(meta_test_losses_after_update)))
 
 
         #Eval
 
         if not meta_epoch % 10:
-            eval_metrics = evaluate(model, eval_data, model_dir, dataset_params, device)
+            eval_metrics, total_loss_eval, eval_avg_goal_acc, eval_joint_goal_acc, avg_slot_precision = evaluate(model, validation_data, model_dir, dataset_params, device)
 
 
+        #Using loss to do HP tuning
+        val_loss = total_loss_eval
+        is_best_loss = val_loss <= best_loss
+
+        # Save model
+        utils.save_checkpoint({'epoch': epoch + 1,
+                               'state_dict': model.state_dict(),
+                               'optim_dict': optimizer.state_dict(),
+                               'loss' : total_loss_eval,
+                               'avg. goal accuracy' : eval_avg_goal_acc,
+                               },
+                                checkpoint=model_dir,
+                                is_best=is_best_loss)
+
+        # If best_eval, best_save_path
+        if is_best_loss:
+            logging.info("- Found new best loss")
+            best_loss = val_loss
+
+            # Save best val metrics in a json file in the model directory
+            best_json_path = os.path.join(
+                model_dir, "metrics_val_best_weights.pkl")
+            utils.save_dict_to_pkl(eval_metrics, best_json_path)
+
+        # Save latest val metrics in a json file in the model directory
+        last_json_path = os.path.join(
+            model_dir, "metrics_val_last_weights.pkl")
+        utils.save_dict_to_pkl(eval_metrics, last_json_path)
 
 if __name__ == '__main__':
 
@@ -135,14 +170,16 @@ if __name__ == '__main__':
 
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-    # first load parameters from params.json
+    # first load parameters from meta_params.json
 
     parser = ArgumentParser()
-    parser.add_argument('--data_dir', default='data/', help='Directory which contain the dialogue dataset')
+    parser.add_argument('--data_dir', default='final_maml_dataset/', help='Directory which contain the dialogue dataset')
     parser.add_argument('--model_dir', default='experiments/', help="Directory containing saved model binaries/\
             train logs")
     parser.add_argument('--config_file', default=False, help='Optionally specify a config file in json format')
     args = parser.parse_args()
+
+
 
     USING_MAML_CONFIGFILE = args.config_file
 
@@ -151,7 +188,7 @@ if __name__ == '__main__':
         candidate_vocabulary = json.load(cand_vocab)['1']
 
     if USING_MAML_CONFIGFILE:
-        json_path = os.path.join(args.model_dir, 'params.json')
+        json_path = os.path.join(args.model_dir, 'meta_params.json')
         assert os.path.isfile(json_path), "No json config file gound at {}".format(json_path)
         params = utils.read_json_file(json_path)
         model_params = params['model']
@@ -202,9 +239,9 @@ if __name__ == '__main__':
     logging.info("Loading the datasets...")
 
     #
-    trainandtest_data_files = {d: {
+    trainandval_data_files = {d: {
                 'train_file_path': '{}_hyst_train.pkl'.format(d),
-                'test_file_path': '{}_hyst_val.pkl'.format(d),
+                'val_file_path': '{}_hyst_val.pkl'.format(d),
             } for d in domains }
 
 
@@ -226,5 +263,5 @@ if __name__ == '__main__':
     # Train the model
     logging.info("Starting MAML for {} meta epoch(s)".format(meta_training_params['meta_epochs']))
 
-    train_and_eval_maml(model, trainandtest_data_files, None, optimizer, meta_optimizer, args.model_dir, args.data_dir,
+    train_and_eval_maml(model, trainandval_data_files, None, optimizer, meta_optimizer, args.model_dir, args.data_dir,
             training_params, dataset_params, meta_training_params, model_params, device)
